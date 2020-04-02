a5fa867304a301eed83c4736b06528f2bf9ca804
Ruud765 <61196871+Ruud765@users.noreply.github.com>
2020-04-02 20:24:36 +0200

    Add files via upload

--- main.go
+++ main.go
@@ -12,17 +12,19 @@ type GData struct {
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseGlob("templates/*"))
+	tpl = template.Must(template.ParseGlob("signup.html"))
 }
 
 func main() {
 
 	http.HandleFunc("/signup", signup)
+	http.ListenAndServe(":8080", nil)
+
 }
 
 func signup(w http.ResponseWriter, r *http.Request) {
 	gd := GData{
 		Title: "SIGNUP",
 	}
-	tpl.ExecuteTemplate(w, "signup.gohtml", gd)
+	tpl.ExecuteTemplate(w, "signup.html", gd)
 }
